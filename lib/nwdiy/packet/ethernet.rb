5089ebc76f863df99be020c55e7b92889a124b6c
KASHIMA Hiroaki <kashima@jp.fujitsu.com>
2016-03-01 13:53:46 +0900

    data-type evaluation

--- lib/nwdiy/packet/ethernet.rb
+++ lib/nwdiy/packet/ethernet.rb
@@ -41,6 +41,8 @@ class NWDIY
       end
 
       ################################################################
+      # 各フィールドの値の操作
+      ################################################################
       # 宛先 MAC
       def dst=(mac)
         @dst = NWDIY::PKT::MAC.new(mac)
@@ -49,7 +51,7 @@ class NWDIY
         @dst
       end
 
-      ################################################################
+      ################
       # 送信先 MAC
       def src=(mac)
         @src = NWDIY::PKT::MAC.new(mac)
@@ -58,68 +60,58 @@ class NWDIY
         @src
       end
 
-      ################################################################
+      ################
       # Ethernet の type あるいは IEEE802.3 の length
-      def type=(ethertype)
-        tmp = NWDIY::PKT::Ethernet::Type.new(ethertype)
-        if tmp > 1500
-          @type = tmp
-          # self.data = XXX 新しい型に合わせて読み直す
+      TYPES = {
+        'IPv4' => 0x0800,
+        'ARP'  => 0x0806,
+        'IPv6' => 0x86dd,
+        'VLAN' => 0x8100 }
+      def type=(val)
+        val = TYPES[val] || val
+        val = NWDIY::PKT::UINT16.new(val)
+        if val > 1500
+          @type = val
         else
           @type = nil
         end
+        # 型が変わるなら、フォーマット適用し直し
+        (@data && @data.class != self.dataKlass) and
+          self.data = @data.to_pkt
       end
       def type
-        @type and return @type
-        # ↑ Ethernet
-        # ↓ 802.3
-        NWDIY::PKT::Ethernet::Type.new(@data ?
-                                       @data.length :
-                                       0).to_i
-      end
-      class Type < NWDIY::PKT::UINT16
-        TYPES = {
-          'IPv4' => 0x0800,
-          'ARP'  => 0x0806,
-          'IPv6' => 0x86dd,
-          'VLAN' => 0x8100 }
-
-        # TYPES に定義した固定文字列か、あるいは数値かバイナリ
-        def initialize(val)
-          super(TYPES[val] || val)
-        end
-
-        # 値ならデータの型を返す
-        # (autoload が効くように、配列やハッシュにせずコードで列挙する)
-        def klass
-          case self
-          when 0x0800 then NWDIY::PKT::IPv4
-          when 0x0806 then NWDIY::PKT::ARP
-          when 0x86dd then NWDIY::PKT::IPv6
-          when 0x8100 then NWDIY::PKT::VLAN
-          else             NWDIY::PKT::Binary
-          end
-        end
+        # Ethernet       802.3
+        # ↓              ↓
+        @type || (@data ? @data.length : 0)
       end
 
-      ################################################################
+      ################
       # データ部
+      def dataKlass
+        # (autoload が効くように、配列やハッシュにせずコードで列挙する)
+        case @type
+        when 0x0806 then NWDIY::PKT::ARP
+        # when 0x0800 then NWDIY::PKT::IPv4
+        # when 0x86dd then NWDIY::PKT::IPv6
+        # when 0x8100 then NWDIY::PKT::VLAN
+        else             NWDIY::PKT::Binary
+        end
+      end
       def data=(body)
-        # XXX @type があれば、そこから型を求める
-        @data = NWDIY::PKT::Binary.new(body)
+        @data = self.dataKlass.new(body)
       end
       def data
         @data
       end
 
+      ################################################################
+      # その他の諸々
       def length
         14 + @data.length
       end
-
       def to_s
         '[Ethernet dst=' + @dst.to_s + ', src=' + @src.to_s + ', type=' + self.type.to_s + ', data=' + @data.to_s + ']'
       end
-
       def to_pkt
         self.dst.to_pkt + self.src.to_pkt + self.type.to_pkt + self.data.to_pkt
       end
