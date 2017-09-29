860605d91be0e2b39754493e3096ddebba848cba
KASHIMA Hiroaki <kashima@jp.fujitsu.com>
2017-09-29 18:07:53 +0900

    simple spec/nwdiy/packet_spec.rb

--- spec/nwdiy/packet_spec.rb
+++ spec/nwdiy/packet_spec.rb
@@ -9,13 +9,11 @@
 #
 # 単純なパケットの例
 # class SubPacket < Nwdiy::Packet
-#   def_field Nwdiy::Packet::Mac,  :dst
-#   def_field Nwdiy::Packet::Mac,  :src
+#   def_field Nwdiy::Packet::Mac,  :dst, :src
 #   def_field :uint16,             :type
-#   def_field :datatype            :data
 #
-#   def datatype(data)
-#     return Nwdiy::Packet::IPv4
+#   def parse_data(data)
+#     [@field1, @field2] = data.unpack("...")
 #   end
 #
 #   def to_s
@@ -29,13 +27,21 @@
 #   end
 # end
 #
-# 上記のように 「"def_field" 型, フィールド名, オプション」と定義します。
-# 事前に型が決まらない箇所では、型を求めるインスタンスメソッドを
-# 記載することも可能です。
+# 一般的にパケットは、前半に固定長フィールドが配置され、
+# 後半に可変長フィールドが配置されています。
+#
+# サブクラスを定義するときは、前半の固定長フィールド def_field 文で
+# ひとつづつ定義し、後半の可変長フィールドを parse_data メソッドで
+# 一気にパースします。
+#
+# 固定長フィールドの各項は、上記の例のように
+# 「"def_field" 型, フィールド名」と定義します。
+# フィールド名と同一名称の、インスタンス変数, 参照メソッド, 代入メソッド
+# が生成されます。
 #
 # 型には以下の種類があります
 #
-#   固定長バイト列として uint8, uint16, uint32, uint64 があります。
+#   固定長バイト列として :uint8, :uint16, :uint32, :uint64 があります。
 #   このフィールドを参照すると、設定された値を Integer で返します。
 #   このフィールドに代入する時は、Integer あるいはバイト列で設定できます。
 #
@@ -44,25 +50,6 @@
 #   返します。
 #   このフィールドに代入する時は、指定したサブクラスのインスタンス
 #   あるいはバイト列で設定できます。
-#
-#   型が可変なフィールドについては、型を求めるインスタンスメソッドを
-#   指定することができます。
-#   このフィールドを参照すると、何らかの Nwdiy::Packet サブクラスの
-#   インスタンスを返します。
-#   このフィールドに Nwdiy::Packet サブクラスのインスタンスを代入
-#   することができます。このとき、型を求めるインスタンスメソッドは
-#   インスタンスのクラスが適切かどうかチェックし、適切であれば
-#   それに合わせてインスタンス内の他のフィールドも修正します。
-#   このフィールドにバイト列を代入することもできます。このとき
-#   型を求めるインスタンスメソッドによって、型が決まります。
-#
-# オプションはハッシュ形式で設定します。
-# 以下のパラメーターを指定できます。
-#
-#   length: 配列サイズ
-#      def_array で定義する配列のサイズが固定である場合に
-#      その配列サイズを記載します。
-#      def_field では効力を持ちません。
 #
 #【このクラスの特異メソッド】
 #
@@ -83,9 +70,12 @@
 # template -> String
 #    インスタンスを pack してバイト列を作ったり、
 #    バイト列を unpack してインスタンスを作るための
-#    pack テンプレート文字列を返します。
+#    pack テンプレート文字列を返してください。
 #    省略時値は def_field などから自動生成します。
-#    変更したくなると思えないので、隠しメソッドでいいかな・・・
+#
+# size -> Integer
+#    フィールドが固定長である場合に、そのバイトサイズを返してください。
+#    省略時値は def_field などから自動生成します。
 #
 #【サブクラスのインスタンスメソッド】
 #
@@ -127,38 +117,23 @@ RSpec.describe Nwdiy::Packet do
     expect(Nwdiy::Packet.calc_cksum(*data)).to be(0xffff)
   end
 
-  # it 'has a direction' do
-  #   data =
-  #     "\x00\x00\x0e\x00\x00\x01" + # eth dst
-  #     "\x00\x00\x0e\x00\x00\x02" + # eth src
-  #     "\x08\x00" +                 # eth type
-  #     "\x45\x00\x00\x54\xf1\x18" + # IPv4 vhl, tos, len, id
-  #     "\x40\x00\x40\x01\x31\x80" + # IPv4 frag, ttl, proto, cksum
-  #     "\x0a\x00\x02\x0f" +         # IPv4 src
-  #     "\x0a\x00\x02\x02" +         # IPv4 dst
-  #     "\x08\x00\xc3\x7c" +         # ICMP type, code, cksum
-  #     "\x24\x5b\x00\x01" +         # ICMP id, seq
-  #     "\x9e\x3e\x9d\x59\0\0\0\0" + # ICMP timestamp
-  #     "\x10\xbc\x05\x00\0\0\0\0" +         # ICMP data
-  #     "\x10\x11\x12\x13\x14\x15\x16\x17" + # ICMP data
-  #     "\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" + # ICMP data
-  #     "\x20\x21\x22\x23\x24\x25\x26\x27" + # ICMP data
-  #     "\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f" + # ICMP data
-  #     "\x20\x31\x32\x33\x34\x35\x36\x37" + # ICMP data
-  #     ""
-  #   pkt = Nwdiy::Packet::Ethernet.new(data)
-  #   expect(pkt.class).to be Nwdiy::Packet::Ethernet
-  #   expect(pkt.auto_compile).to be true
-  #   expect(pkt.data.auto_compile).to be true
-  #   expect(pkt.direction).to be :UNKNOWN
-  #   expect(pkt.data.direction).to be :UNKNOWN
-
-  #   pkt.auto_compile = false
-  #   expect(pkt.auto_compile).to be false
-  #   expect(pkt.data.auto_compile).to be false
-
-  #   pkt.dir_to_right
-  #   expect(pkt.direction).to be :LEFT_TO_RIGHT
-  #   expect(pkt.data.direction).to be :LEFT_TO_RIGHT
-  # end
+  it "create an packet field" do
+    class Sample < Nwdiy::Packet
+      def_field Nwdiy::Packet::Mac, :dst, :src
+      def_field :uint16, :type
+      def parse_data(data)
+        @data = data
+      end
+    end
+    src = "\0e\00\00\00\00\01"
+    dst = "\0e\00\00\00\00\02"
+    type = "\08\00"
+    data = "Hello World"
+    smpl = Sample.new(src + dst + type + data)
+    expect(smpl.class).to be(Sample)
+    expect(smpl.src).to be(src)
+    expect(smpl.dst).to be(dst)
+    expect(smpl.type).to be(type)
+    expect(smpl.data).to be(data)
+  end
 end
