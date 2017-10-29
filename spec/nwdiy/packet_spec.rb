78cd452dee8fe081b25d25b3bc71f65679958587
KASHIMA Hiroaki <hiro@karoshima.org>
2017-10-29 16:52:17 +0900

    new def_head & def_body

--- spec/nwdiy/packet_spec.rb
+++ spec/nwdiy/packet_spec.rb
@@ -16,40 +16,80 @@
 # end
 #
 # class SubPacket < Nwdiy::Packet
-#   def_field Nwdiy::Packet::Mac,  :dst, :src
-#   def_field :uint16,             :type
 #
-#   def_typed_field :type, nil, :data,
-#     0x0800 => "Nwdiy::Packet::IPv4",
-#     0x0806 => "Nwdiy::Packet::ARP",
-#     0x8100 => "Nwdiy::Packet::VLAN",
-#     0x86dd => "Nwdiy::Packet::IPv6"
+#   ################
+#   # まずヘッダフィールドを順に定義します
+#   # インスタンスメソッドで参照と代入が可能になります
+#   # ちなみに実体は @nwdiy_field[フィールド名シンボル] です
 #
-#   def_trailer :fcs
+#   def_head :uint32 :line1
+#   def_head :uint16 :length
+#   def_head :uint8  :next, :hlim
+#   def_head Nwdiy::Packet::IPv6Addr, :src, :dst
+#
+#   ################
+#   # 次にボディデータを定義します
+#   # インスタンスメソッドで参照が可能になります
+#   # 代入はパケットクラスごとに各自定義してください
+#   # 実体は @nwdiy_field[フィールド名シンボル] です
+#
+#   def_body :data1, :data2, ...
+#   def data1=(xxx)
+#     @nwdiy_field[:data1] = ...
+#   end
+#
+#   ################
+#   # 多くの種類のパケットでは、ヘッダ上のタイプ値に沿って
+#   # ボディデータの型が決まります。
+#   # タイプ値とボディの型の対応表を def_body_type で定義しておくと
+#   # ボディデータ代入メソッド定義のなかで、
+#   # クラスメソッド body_type() を使うことで、
+#   # body_type(フィールド名シンボル, タイプ値) でクラスが分かり、
+#   # body_type(フィールド名シンボル, インスタンス) でタイプ値が分かります。
+#
+#   def_body_type :data1,
+#                 1,  "Nwdiy::Packet::ICMP",
+#                 6,  "Nwdiy::Packet::TCP",
+#                 14  "Nwdiy::Packet::UDP"
+#
+#   def data2=(xxx)
+#     case xxx
+#     when String
+#       @nwdiy_field[:data2] = self.class.body_type(:next, xxx)
+#     when Nwdiy::Packet
+#       self.type = self.class.body_type(xxx)
+#     end
+#   end
+#
+#   ################
+#   # ヘッダやボディの再定義
+#   # チェックサムや長さなど、上記の定義で生成されるメソッドを使わず
+#   # 変更したいときは、def_head, def_body のあとに続けて
+#   # メソッドを定義してください。
+#   # フィールドに対応するデータは @nwdiy_field[フィールド名シンボル] です
+#
+#   def cksum
+#     ...
+#   end
+#
+#   ################
+#   # インスタンスの実データ化, 可視化
+#   # インスタンスをパケットデータに変換する to_s や
+#   # インスタンスを可視化する inspect は
+#   # 上記の def_head, def_body によって生成されています。
+#   # 変更したいときは、def_head, def_body のあとに続けて
+#   # メソッドを再定義してください。
 #
 #   def to_s
-#     dst.to_s + src.to_s + vlan.map{|v|v.to_s} + type.to_s + data.to_s
-#   end
-#   def bytesize
-#     to_s.bytesize
+#     ...
 #   end
 #   def inspect
-#     "[xx:xx:xx:xx:xx:xx => xx:xx:xx:xx:xx:xx [xxx]]"
+#     ...
 #   end
-# end
 #
-# 一般的にパケットは、固定長フィールドと後続データで構成されます。
-# 稀に後続データのあとに「トレーラー」と称するデータを付けて
-# 認証などに利用することもあります。
+################################################################
 #
-# パケットのクラスを定義するときは、Nwdiy::Packet を継承します。
-# その中では、まず固定長フィールドを def_field 文で定義し、
-# 次に後続データとトレーラーをそれぞれ def_typed_field 文と
-# def_trailer 文で定義します。
-# 定義したフィールドは、インスタンスメソッドや
-# インスタンス変数として参照できます。
-#
-# def_field で使用する型には以下の種類があります
+# def_head で使用する型には以下の種類があります
 #
 #   固定長バイト列として :uint8, :uint16, :uint32 があります。
 #   このフィールドを参照すると、設定された値を Integer で返します。
@@ -65,22 +105,46 @@
 #   このフィールドに代入する時は、指定したサブクラスのインスタンス
 #   あるいはバイト列で設定できます。
 #
-# 後続データは一般的に、固定長フィールド中どれかの値によって
-# 型とサイズが決まります。
-#
-
-
-# ありがちなパケットの形態として、タイプ値によって後続データの型が決まる
-# というものがあります。
-# そのときは def_field ではなく def_typed_field を使用して、
-# タイプとして扱うフィールドのフィールド名シンボル,
-# データとして扱うフィールドのフィールド名シンボル,
-# 続けてタイプとデータの対応表をハッシュで設定します。
+# ボディデータの型は一般的に、ヘッダフィールド中どれかの値によって
+# 型とサイズが決まります。上述の def_body_type で対応表を定義して、
+# body_type() で対応表を参照してください。
 #
 #【このクラスの特異メソッド】
 #
 # calc_cksum(*args) -> Integer (16bit)
 #    ひとつ以上の引数からチェックサムを計算します。
+#
+#【サブクラスに定義する特異メソッド】
+#
+# def_head(type, *fields)
+#
+#    type 型の変数 name を定義します。
+#    type には以下いずれかのシンボルを使うことができます。
+#        :uint8     8 bit の Integer
+#        :uint16   16 bit の Integer
+#        :uint32   32 bit の Integer
+#        :byteN    N byte の String
+#    type はこれ以外に、Nwdiy::Packet の子クラスを指定することもできます。
+#
+#    fields には任意のシンボルを使うことができます。
+#    ここに指定したシンボルはフィールド名となります。
+#    そしてインスタンスメソッドとして代入や参照できるようになります。
+#    インスタンスメソッド to_s や inspect に反映されます。
+#
+# def_body(*fields)
+#
+#    fields には任意のシンボルを使うことができます。
+#    ここに指定したシンボルはフィールド名となります。
+#    そしてインスタンスメソッドとして参照できるようになります。
+#    インスタンスメソッド to_s や inspect に反映されます。
+#
+# def フィールド名=(xxx)
+#   @nwdiy_field[フィールド名シンボル] = ...
+# end
+#
+#    def_body では代入のインスタンスメソッドが生成されません。
+#    なので自分で代入メソッドを定義し、そのなかで
+#    @nwdiy_field[フィールドシンボル] に代入してください。
 #
 #【サブクラスで使える特異メソッド】
 #
@@ -91,47 +155,20 @@
 # new(Hash) -> SubPacket
 #    フィールド名ごとに値を指定して作成したインスタンスを返します。
 #
-#【サブクラスに定義する特異メソッド】
+# body_type(フィールド名シンボル, 数値) -> Class
 #
-# def_field(type, *name)
+#    ヘッダフィールドに定義されたタイプ値から
+#    ボディデータのクラスを返します。
 #
-#    type 型の変数 name を定義します。
-#    type には以下いずれかのシンボルを使うことができます。
-#        :uint8     8 bit の Integer
-#        :uint16   16 bit の Integer
-#        :uint32   32 bit の Integer
-#        :byteN    N byte の String
-#    type はこれ以外に、Nwdiy::Packet の子クラスを指定することもできます。
+# body_type(フィールド名シンボル, インスタンス) -> Integer
 #
-#    name には任意のシンボルを使うことができます。
-#    ここに指定したシンボルはフィールド名となり、
-#    インスタンス変数として、そしてインスタンスメソッドとして、
-#    代入や参照ができるようになります。
-#
-# def_typed_field(type, length, name, hash)
-#
-#    type には def_field で指定した数値フィールド名をシンボルで指定します。
-#    TCP のポート番号のように複数のフィールドがある場合は、配列にします。
-#
-#    length にはデータの長さを指定します。
-#    コーディング時に分かっている場合には整数を指定します。
-#    他のフィールドから読み取れるときは、そのフィールド名シンボルを指定します。
-#    分からない場合には nil を指定します。
-#
-#    name は def_field と同様に変数名となります。
-#
-#    hash には type 値と name クラスの対応表を指定します。
-#    このハッシュには、クラスそのものではなく文字列で記載することもできます。
-#    文字列で記載することで、起動時にライブラリをすべて読み込んで
-#    しまうのでなく、必要なクラスだけ遅延読み込みにすることができます。
-#
-# def_trailer(name)
-#
-#    残ったデータがバイト列 (String) で格納されます。
+#    ボディデータに使用するインスタンスから
+#    ヘッダフィールドのタイプ値として使うべき数値を返します
 #
 # bytesize -> String
-#    固定長のサブクラスでは、そのバイト長を返してください。
-#    可変長のサブクラスでは、nil を返してください。
+#    サブクラス定義が def_head 定義のみで def_body 定義がない場合、
+#    パケット長が分かるのでその長さを返します。
+#    def_body 定義もある場合は、パケット長が分からないので nil を返します。
 #
 #【サブクラスのインスタンスメソッド】
 #
@@ -146,11 +183,10 @@
 #    def_array で定義したフィールド配列に string から得た値を設定します。
 #    設定した値と余った string を返します。
 #
-# フィールド名=(value) -> 値
-#    value が String ではない場合、
+# フィールド名=(value) -> value
 #    def_field で定義したフィールドに value を設定し、それを返します。
 #
-#【サブクラスに定義するインスタンスメソッド】
+#【サブクラスで使えるインスタンスメソッド】
 #
 # to_s -> String
 #    パケットをバイト列 (String) に変換します。
@@ -178,36 +214,39 @@ RSpec.describe Nwdiy::Packet do
   it "creates an packet field" do
 
     class Sample01 < Nwdiy::Packet
-      def_field :byte6, :dst, :src
-      def_field :uint16, :type
-      def parse_data(data)
-        @data = data
+      def_head :byte6, :dst, :src
+      def_head :uint16, :type
+      def_body :data
+      def data=(val)
+        @nwdiy_field[:data] = val
       end
-      attr_accessor :data
     end
 
     dst = "\x00\x00\x0e\x00\x00\x01"
     src = "\x00\x00\x0e\x00\x00\x02"
     type = "\x08\x00"
     data = "Hello World"
-    smpl = Sample01.new(dst + src + type + data)
+    pkt = dst + src + type + data
+    smpl = Sample01.new(pkt)
 
-    expect(smpl.class).to be(Sample01)
+    expect(smpl).to be_a Sample01
     expect(smpl.dst).to eq(dst)
     expect(smpl.src).to eq(src)
     expect(smpl.type).to eq(0x0800)
     expect(smpl.data).to eq(data)
+    expect(smpl.to_s).to eq(pkt)
+    expect(smpl.bytesize).to eq(pkt.bytesize)
   end
 
   it "creates an packet which include Nwdiy::Packet parts" do
 
     class Sample02 < Nwdiy::Packet
-      def_field Nwdiy::Packet::MacAddr, :dst, :src
-      def_field :uint16, :type
-      def parse_data(data)
-        @data = data
+      def_head Nwdiy::Packet::MacAddr, :dst, :src
+      def_head :uint16, :type
+      def_body :data
+      def data=(xxx)
+        @data = xxx
       end
-      attr_accessor :data
     end
 
     dst = "\x00\x00\x0e\x00\x00\x01"
@@ -216,7 +255,7 @@ RSpec.describe Nwdiy::Packet do
     data = "Hello World"
     smpl = Sample02.new(dst + src + type + data)
 
-    expect(smpl.class).to be(Sample02)
+    expect(smpl).to be_a(Sample02)
     expect(smpl.dst.to_s).to eq(dst)
     expect(smpl.src.to_s).to eq(src)
     expect(smpl.dst.inspect).to eq("00:00:0e:00:00:01")
@@ -225,36 +264,4 @@ RSpec.describe Nwdiy::Packet do
     expect(smpl.data).to eq(data)
   end
 
-  it "creates an packet which uses def_typed_field" do
-    class Nwdiy::Packet
-      autoload(:MacAddr,  'nwdiy/packet/macaddr')
-      autoload(:IPv4Addr, 'nwdiy/packet/ipv4addr')
-    end
-    class Sample03 < Nwdiy::Packet
-      def_field :uint8, :type
-      def_typed_field :type, nil, :addr,
-                      1 => "Nwdiy::Packet::MacAddr",
-                      2 => "Nwdiy::Packet::IPv4Addr"
-      def_trailer :fcs
-    end
-
-    smpl1 = Sample03.new("\x01" + "\x80\x81\x82\x83\x84\x85")
-    expect(smpl1).to be_a Sample03
-    expect(smpl1.type).to eq 1
-    expect(smpl1.addr).to be_a Nwdiy::Packet::MacAddr
-    expect(smpl1.fcs).to be_nil
-
-    smpl2 = Sample03.new("\x02" + "\x80\x81\x82\x83\x84\x85")
-    expect(smpl1).to be_a Sample03
-    expect(smpl1.type).to eq 2
-    expect(smpl1.addr).to be_a Nwdiy::Packet::IPv4Addr
-    expect(smpl1.fcs).to eq "\x84\x85"
-
-    smpl3 = Sample03.new
-    smpl3.addr = Nwdiy::Packet::MacAddr.new("00:00:0e:00:00:01")
-    expect(smpl3.type).to be 1
-
-    smpl3.addr = Nwdiy::Packet::IPv4Addr.new("1.1.1.1")
-    expect(smpl3.type).to be 2
-  end
 end
