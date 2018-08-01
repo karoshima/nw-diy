3276a8a336038aeee48957e6bfe7b98c447bb887
KASHIMA Hiroaki <kashima@jp.fujitsu.com>
2018-08-01 14:40:19 +0900

    #27 EtherIP initialize

--- lib/nwdiy/func/etherip.rb
+++ lib/nwdiy/func/etherip.rb
@@ -10,6 +10,14 @@
 #    instance methods for instances under Nwdiy::Func::EtherIP
 #    typically, IPv4 and IPv6
 ################################################################
+# irb> ipv4.etherip(peer: "192.0.2.2")
+#
+#    (1) IPv4 instance creates EtherIP instance,
+#        which handles IP proto 97 and peer nodes.
+#    (2) @eip creates peer node 192.0.2.2.
+#    (3) the new peer node creates Ethernet instance on it.
+#    
+################################################################
 
 require 'thread'
 require 'nwdiy/func/ethernet'
@@ -21,6 +29,77 @@ module Nwdiy
     # create an EtherIP instance
 
     class EtherIP
+
+      IP_PROTO_ETHERIP = 97
+
+      include Nwdiy::Func
+      include Nwdiy::Debug
+
+      def initialize(klass)
+        name = 'EtherIP'
+        debug(name)
+        super(name)
+        @addrClass = klass
+        @node = Hash.new { |hash,key| hash[key] = EtherIPNode.new(self, key) }
+      end
+    end
+
+    module EtherIPReceiver
+      def etherip
+        eip = self[EtherIP::IP_PROTO_ETHERIP]
+        unless eip
+          case self
+          when Nwdiy::Func::IPv4
+            klass = Nwdiy::Packet::IPv4Addr
+          else
+            raise "Unknown Function class #{self.class}"
+          end
+          eip = self[EtherIP::IP_PROTO_ETHERIP] = EtherIP.new(klass)
+          eip.lower = self
+        end
+        return eip
+      end
+    end
+
+    class IPv4
+      include EtherIPReceiver
+    end
+
+    class EtherIP
+      public
+      def lower=(instance)
+        if instance
+          debug "@instance_lower = #{instance}"
+          @instance_lower = instance
+#          self.thread_start
+        else
+#          self.thread_stop
+          @instance_lower = nil
+        end
+      end
+
+      ################################################################
+      # upper layers
+      def [](addr)
+        unless addr.kind_of?(@addrClass)
+          addr = @addrClass.new(addr)
+        end
+        return @node[addr]
+      end
+    end
+
+    ################################################################
+    # create an EtherIP peer node instance
+
+    class EtherIPNode < Ethernet
+
+      def initialize(etherip, addr)
+        name = "EtherIP(#{addr.inspect})"
+        debug(name)
+        super(name)
+        @etherip = etherip
+        @addr = addr
+      end
     end
   end
 end
--- spec/nwdiy/func/etherip_spec.rb
+++ spec/nwdiy/func/etherip_spec.rb
@@ -10,5 +10,29 @@ require "spec_helper"
 
 Thread.abort_on_exception = true
 
-# RSpec.describe Nwdiy::Func::EtherIP do
-# end
+RSpec.describe Nwdiy::Func::EtherIP do
+
+  it 'can create EtherIP device' do
+    eip = Nwdiy::Func::EtherIP.new(Nwdiy::Packet::IPv4Addr)
+    skip('メソッドはあとで作る')
+    # it must be able to send/recv packets
+    expect(eip.respond_to?(:sendpkt)).to eq true
+    expect(eip.respond_to?(:recvpkt)).to eq true
+    # it must be able to get sent packets, able to push a received packets
+    expect(eip.respond_to?(:push)).to eq true
+    expect(eip.respond_to?(:pop)).to eq true
+  end
+
+  it 'can create Ethernet device from an EtherIP' do
+    eip = Nwdiy::Func::EtherIP.new(Nwdiy::Packet::IPv4Addr)
+    eth = eip["192.168.1.2"]
+    expect(eth).to be_kind_of(Nwdiy::Func::Ethernet)
+  end
+
+  it 'can create Ethernet device from an IPv4 device' do
+    ip1 = Nwdiy::Func::IPv4.new("ip1", local: "192.168.1.1/24")
+    eth = ip1.etherip["192.168.1.2"]
+    expect(eth).to be_kind_of(Nwdiy::Func::Ethernet)
+    expect(eth.to_s).to eq "EtherIP(192.168.1.2)"
+  end
+end
